56608a9125d81fa5bfe3fd3aa3299f42c6a2d391
JandroThielman <9020498@student.zadkine.nl>
2023-12-01 12:52:28 +0100

    jandro

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,10 @@
-setInterval(document.getElementById("Block").style.animation = "Move 2s linear infinite normal", 100);
+const winkelmandje = ["Bananen", "Whiskey", "Chips", "Bonen", "Aardappelen"];
+
+let text = "";
+for (let i = 1; i < winkelmandje.length; i++) {
+  text += winkelmandje[i];
+}
+
+document.getElementById("j").innerHTML = "Er zitten " + winkelmandje.length + " producten in uw winkelmand.";
+
+document.getElementById("a").innerHTML = "Uw producten zijn: " + text + ".";
